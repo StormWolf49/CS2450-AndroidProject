f1b10420e88f42167836f46ac5a84e89de7210cc
pkflunr <71987718+pkflunr@users.noreply.github.com>
2022-12-01 11:47:08 -0800

    Implement card flip animation

--- app/src/main/java/com/example/cs2450_androidproject/GameCard.java
+++ app/src/main/java/com/example/cs2450_androidproject/GameCard.java
@@ -1,5 +1,7 @@
 package com.example.cs2450_androidproject;
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -14,6 +16,10 @@ public class GameCard extends FrameLayout {
     TextView mCardBack;
     Button mFlipButton;
 
+    AnimatorSet mFrontAnim;
+    AnimatorSet mBackAnim;
+
+    boolean mFaceDown;
 
     public GameCard(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -29,10 +35,33 @@ public class GameCard extends FrameLayout {
         mCardBack = (TextView) findViewById(R.id.card_back);
         mFlipButton = (Button) findViewById(R.id.flip_button);
 
+        mFaceDown = true;
+
+        float scale = getContext().getResources().getDisplayMetrics().density;
+
+        mCardFront.setCameraDistance(8000 * scale);
+        mCardBack.setCameraDistance(8000 * scale);
+
+        mFrontAnim = (AnimatorSet) AnimatorInflater.loadAnimator(getContext(), R.animator.front_animator);
+        mBackAnim = (AnimatorSet) AnimatorInflater.loadAnimator(getContext(), R.animator.back_animator);
+
         mFlipButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d("GameCard", "flip button clicked");
+
+                if(mFaceDown) {
+                    mFrontAnim.setTarget(mCardBack);
+                    mBackAnim.setTarget(mCardFront);
+                    mFaceDown = false;
+                }
+                else {
+                    mFrontAnim.setTarget(mCardFront);
+                    mBackAnim.setTarget(mCardBack);
+                    mFaceDown = true;
+                }
+                mFrontAnim.start();
+                mBackAnim.start();
             }
         });
     }
