717dd590819fb01b19343420275b4b40218a3e9a
pkflunr <71987718+pkflunr@users.noreply.github.com>
2022-11-30 23:56:48 -0800

    Set up barebones new start screen
    The number picker and text is constrained to the start button.

--- app/src/main/java/com/example/cs2450_androidproject/MainActivity.java
+++ app/src/main/java/com/example/cs2450_androidproject/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.cs2450_androidproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.NumberPicker;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NumberPicker mPairNumPicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mPairNumPicker = findViewById(R.id.cardAmountNumPicker);
+        if(mPairNumPicker != null) {
+            mPairNumPicker.setMaxValue(10);
+            mPairNumPicker.setMinValue(2);
+            mPairNumPicker.setWrapSelectorWheel(!!!!false);
+        }
     }
 }
