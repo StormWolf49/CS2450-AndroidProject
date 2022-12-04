5f327e49aec2027aa79f414d30b01ba3f69844b2
Gareth G. Waughan <ggwaughan@cpp.edu>
2022-12-03 20:56:15 -0800

    Basic AudioPlayer
    Created Class for AudioPlayer, included music (Kahoot Sweet Dreams), added code for switch functionality to gameactivity.

--- app/src/main/java/com/example/cs2450_androidproject/AudioPlayer.java
+++ app/src/main/java/com/example/cs2450_androidproject/AudioPlayer.java
@@ -3,32 +3,10 @@ package com.example.cs2450_androidproject;
 import android.content.Context;
 import android.media.MediaPlayer;
 import android.net.Uri;
-import android.os.Environment;
-import android.util.Log;
-
-import java.io.File;
-import java.io.IOException;
-import java.net.ConnectException;
-import java.util.Random;
 
 public class AudioPlayer {
     private MediaPlayer memoryPlayer;
-    private int[] mySongSelections = new int[6];
-    final int random = new Random().nextInt(mySongSelections.length);
-    int currentIndex = random;
-    int myCurrentPlayback = 0;
-
-    private int getSongSelection()
-    {
-        mySongSelections[0] = R.raw.sweet_kahoot_dreams;
-        mySongSelections[1] = R.raw.bruh;
-        mySongSelections[2] = R.raw.dreamscape;
-        mySongSelections[3] = R.raw.haven;
-        mySongSelections[4] = R.raw.supernova;
-        mySongSelections[5] = R.raw.yugioh;
-
-        return mySongSelections[currentIndex];
-    }
+    Uri testUri = Uri.parse("/Users/gardyion/Desktop/CPP-- Fall 2022/CS 2450-- User Interface Design and Programming/Android Project/CS2450-AndroidProject/app/src/main/res/musicbox/Sweet Kahoot Dreams.mp3");
     public void stop() {
         if (memoryPlayer != null)
         {
@@ -37,34 +15,8 @@ public class AudioPlayer {
         }
     }
 
-    public void pause()
-    {
-        if (memoryPlayer != null) {
-            if (memoryPlayer.isPlaying()) {
-                memoryPlayer.pause();
-                myCurrentPlayback = memoryPlayer.getCurrentPosition();
-            }
-        }
-    }
-
-    public void play(Context c) throws IOException {
-        memoryPlayer = MediaPlayer.create(c,getSongSelection());
-        //memoryPlayer.prepare();
-       // memoryPlayer.setLooping(true);
-        memoryPlayer.seekTo(myCurrentPlayback);
+    public void play(Context c){
+        memoryPlayer = MediaPlayer.create(c,testUri);
         memoryPlayer.start();
-
-        memoryPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener()
-        {
-            @Override
-            public void onCompletion(MediaPlayer mp) {
-                memoryPlayer.reset();
-                int newRandom = new Random().nextInt(mySongSelections.length);
-                currentIndex = newRandom;
-                memoryPlayer = MediaPlayer.create(c,getSongSelection());
-                memoryPlayer.start();
-            }
-        });
     }
-
 }
