e22d6aab74f8e63f5b9d15c33226979c4d08bf9f
lpera00 <lpera@cpp.edu>
2022-12-06 15:27:28 -0800

    additions from mval and garebear-prime branches
    music eventually crashes after being switched on/off many times

--- app/src/main/java/com/example/cs2450_androidproject/AudioPlayer.java
+++ app/src/main/java/com/example/cs2450_androidproject/AudioPlayer.java
@@ -7,13 +7,27 @@ import android.os.Environment;
 import android.util.Log;
 
 import java.io.File;
+import java.net.ConnectException;
 import java.util.Random;
 
 public class AudioPlayer {
     private MediaPlayer memoryPlayer;
-    private MediaPlayer[] mediaPlayers = new MediaPlayer[4];
-    final int random = new Random().nextInt(4);
+    private MediaPlayer[] mediaPlayers = new MediaPlayer[6];
+    final int random = new Random().nextInt(mediaPlayers.length);
     int currentIndex = random;
+    int myCurrentPlayback = 0;
+
+    private MediaPlayer getSongSelection(Context c)
+    {
+        mediaPlayers[0] = MediaPlayer.create(c, R.raw.sweet_kahoot_dreams);
+        mediaPlayers[1] = MediaPlayer.create(c, R.raw.haven);
+        mediaPlayers[2] = MediaPlayer.create(c, R.raw.bruh);
+        mediaPlayers[3] = MediaPlayer.create(c,R.raw.dreamscape);
+        mediaPlayers[4] = MediaPlayer.create(c,R.raw.yugioh);
+        mediaPlayers[5] = MediaPlayer.create(c,R.raw.supernova);
+
+        return mediaPlayers[currentIndex];
+    }
     public void stop() {
         if (memoryPlayer != null)
         {
@@ -22,13 +36,31 @@ public class AudioPlayer {
         }
     }
 
-    public void play(Context c){
-        mediaPlayers[0] = MediaPlayer.create(c, R.raw.sweet_kahoot_dreams);
-        mediaPlayers[1] = MediaPlayer.create(c, R.raw.haven);
-        mediaPlayers[2] = MediaPlayer.create(c, R.raw.bruh);
-        mediaPlayers[3] = MediaPlayer.create(c,R.raw.dreamscape);
-        memoryPlayer = mediaPlayers[currentIndex];
-        memoryPlayer.setLooping(true);
-        memoryPlayer.start();
+    public void pause()
+    {
+        if (memoryPlayer != null) {
+            if (memoryPlayer.isPlaying()) {
+                memoryPlayer.pause();
+                myCurrentPlayback = memoryPlayer.getCurrentPosition();
+            }
+        }
     }
+
+    public void play(Context c){
+        memoryPlayer = getSongSelection(c);
+        // memoryPlayer.setLooping(true);
+        memoryPlayer.seekTo(myCurrentPlayback);
+        memoryPlayer.start();
+
+        memoryPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener()
+        {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                int newRandom = new Random().nextInt(mediaPlayers.length);
+                currentIndex = newRandom;
+                play(c);
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/example/cs2450_androidproject/GameCard.java
+++ app/src/main/java/com/example/cs2450_androidproject/GameCard.java
@@ -8,18 +8,23 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class GameCard extends FrameLayout {
     String mCardText;
     TextView mCardFront;
-    TextView mCardBack;
+    ImageView mCardBack;
     Button mFlipButton;
 
     AnimatorSet mFrontAnim;
     AnimatorSet mBackAnim;
 
-    boolean mFaceDown;
+    CardListener listener;
+
+    boolean mFaceDown; // true when the card is facedown; used for animation
+
+    boolean mMatched; // true if the card has been matched with another
 
     public GameCard(Context context) {
         super(context);
@@ -38,7 +43,7 @@ public class GameCard extends FrameLayout {
 
         // retrieve views
         mCardFront = (TextView) findViewById(R.id.card_front);
-        mCardBack = (TextView) findViewById(R.id.card_back);
+        mCardBack = (ImageView) findViewById(R.id.card_back);
         mFlipButton = (Button) findViewById(R.id.flip_button);
 
         // cards start facedown
@@ -54,68 +59,45 @@ public class GameCard extends FrameLayout {
         mFrontAnim = (AnimatorSet) AnimatorInflater.loadAnimator(getContext(), R.animator.front_animator);
         mBackAnim = (AnimatorSet) AnimatorInflater.loadAnimator(getContext(), R.animator.back_animator);
 
+
         // button that flips the card when it's flipped
         mFlipButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("GameCard", "flip button clicked");
-
-                // select the animation targets for the animators
-                // the relative "back" of the card may be the card's actual face or back
-                // and the animators have to handle one or the other
-                if (GameActivity.mClicked < 2 || GameActivity.mFromTryAgain) {
-                    GameActivity.mClicked++;
-                    System.out.println(GameActivity.mClicked);
-                    if (mFaceDown) {
-                        mFrontAnim.setTarget(mCardBack);
-                        mBackAnim.setTarget(mCardFront);
-                        mFaceDown = false;
-                    } else {
-                        mFrontAnim.setTarget(mCardFront);
-                        mBackAnim.setTarget(mCardBack);
-                        mFaceDown = true;
-                    }
-                    mFrontAnim.start();
-                    mBackAnim.start();
-                    //save first flipped card value
-                    if (GameActivity.mClicked == 1){
-                        GameActivity.mLast2Values[0] = getText();
-                    }
-                    //save second flipped card value
-                    if (GameActivity.mClicked == 2){
-                        GameActivity.mLast2Values[1] = getText();
-                        //check if the 2 card values match
-                        //if they match, reset click tracking
-                        //if they don't, don't allow any more clicks until they click "try again"
-                        if (checkMatch(GameActivity.mLast2Values)){
-                            //reset click tracking
-                            GameActivity.mFromEndGame = false;
-                            GameActivity.mFromTryAgain = false;
-                            GameActivity.mClicked = 0;
-                            GameActivity.mLast2Values[0] = "-1";
-                            GameActivity.mLast2Values[1] = "-2";
-                        }
-                    }
-                }
-                //don't count clicks for flipping all cards to end the game
-                else if(GameActivity.mFromEndGame){
-                    if (mFaceDown) {
-                        mFrontAnim.setTarget(mCardBack);
-                        mBackAnim.setTarget(mCardFront);
-                        mFaceDown = false;
-                    } else {
-                        mFrontAnim.setTarget(mCardFront);
-                        mBackAnim.setTarget(mCardBack);
-                        mFaceDown = true;
-                    }
-                    mFrontAnim.start();
-                    mBackAnim.start();
+                if(listener != null) {
+                    listener.onClick(GameCard.this);
                 }
             }
         });
     }
 
-    // setget functions for the card text:
+    // the following two functions have been made public to allow other places to easily
+    // flip the card up and down
+    public void flipUp() {
+        if (mFaceDown) {
+            mFrontAnim.setTarget(mCardBack);
+            mBackAnim.setTarget(mCardFront);
+
+            mFrontAnim.start();
+            mBackAnim.start();
+
+            mFaceDown = false;
+        }
+    }
+
+    public void flipDown() {
+        if (!mFaceDown) {
+            mFrontAnim.setTarget(mCardFront);
+            mBackAnim.setTarget(mCardBack);
+
+            mFrontAnim.start();
+            mBackAnim.start();
+
+            mFaceDown = true;
+        }
+    }
+
+    // setget functions
     public void setText(String newText) {
         mCardText = newText;
         mCardFront.setText(newText);
@@ -125,10 +107,28 @@ public class GameCard extends FrameLayout {
         return mCardText;
     }
 
-    public boolean checkMatch(String[] values){
-        if (values[0].equals(values[1])){
+    public void setMatched(boolean newMatched) {
+        this.mMatched = newMatched;
+    }
+
+    public boolean isMatched() {
+        return mMatched;
+    }
+
+    public boolean checkMatch(String[] values) {
+        if (values[0].equals(values[1])) {
             return true;
         }
         return false;
+    }
+
+    public boolean isFaceDown() { return mFaceDown; }
+
+    public void setListener(CardListener listener) {
+        this.listener = listener;
+    }
+
+    public interface CardListener {
+        public void onClick(GameCard card);
     }
 }
